4972313cdbaa775e1bc2403011c11f952fc73fad
steven <chunyuko85@gmail.com>
2017-12-01 22:15:57 +0800

    change to advanced native

--- app/src/main/java/com/novel/reader/ArticleActivity.java
+++ app/src/main/java/com/novel/reader/ArticleActivity.java
@@ -1,7 +1,19 @@
 package com.novel.reader;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.NativeExpressAdView;
+import com.google.android.gms.ads.VideoController;
+import com.google.android.gms.ads.VideoOptions;
+import com.google.android.gms.ads.formats.MediaView;
+import com.google.android.gms.ads.formats.NativeAd;
+import com.google.android.gms.ads.formats.NativeAdOptions;
+import com.google.android.gms.ads.formats.NativeAppInstallAd;
+import com.google.android.gms.ads.formats.NativeAppInstallAdView;
+import com.google.android.gms.ads.formats.NativeContentAd;
+import com.google.android.gms.ads.formats.NativeContentAdView;
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
 
@@ -34,14 +46,18 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RatingBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class ArticleActivity extends MopubAdFragmentActivity implements DetectScrollViewListener {
 
@@ -92,8 +108,11 @@ public class ArticleActivity extends MopubAdFragmentActivity implements DetectSc
     private ImageView novelImage;
     private boolean adHasShowed = false;
     private AdInterstitialManager interstitialManager;
-    private NativeExpressAdView mAdView2;
-    private NativeExpressAdView mAdView1;
+    private FrameLayout mAdView2;
+    private FrameLayout mAdView1;
+
+    private static final String ADMOB_AD_UNIT_ID = "ca-app-pub-5774496584789671/5182575586";
+    private static final String ADMOB_APP_ID = "ca-app-pub-5774496584789671~4050055547";
 
 
     @Override
@@ -103,6 +122,8 @@ public class ArticleActivity extends MopubAdFragmentActivity implements DetectSc
 
         restorePreValues();
         setViews();
+
+        MobileAds.initialize(this, ADMOB_APP_ID);
 
         ab = getSupportActionBar();
         mBundle = this.getIntent().getExtras();
@@ -243,8 +264,8 @@ public class ArticleActivity extends MopubAdFragmentActivity implements DetectSc
         articleLayout.setBackgroundColor(Setting.getBackgroundModeBackgroundColor(textMode, this));
         layoutProgress.setBackgroundColor(Setting.getBackgroundModeBackgroundColor(textMode, this));
 
-        mAdView1 = (NativeExpressAdView) findViewById(R.id.adView1);
-        mAdView2 = (NativeExpressAdView) findViewById(R.id.adView2);
+        mAdView1 = (FrameLayout) findViewById(R.id.adView1);
+        mAdView2 = (FrameLayout) findViewById(R.id.adView2);
         mAdView1.setVisibility(View.GONE);
         mAdView2.setVisibility(View.GONE);
 
@@ -574,9 +595,198 @@ public class ArticleActivity extends MopubAdFragmentActivity implements DetectSc
         }else{
             mAdView1.setVisibility(View.VISIBLE);
             mAdView2.setVisibility(View.VISIBLE);
-            mAdView1.loadAd(new AdRequest.Builder().build());
-            mAdView2.loadAd(new AdRequest.Builder().build());
+            refreshAd();
         }
+    }
+
+    private void refreshAd() {
+        AdLoader.Builder builder2 = new AdLoader.Builder(this, ADMOB_AD_UNIT_ID);
+
+
+        builder2.forAppInstallAd(new NativeAppInstallAd.OnAppInstallAdLoadedListener() {
+            @Override
+            public void onAppInstallAdLoaded(NativeAppInstallAd ad) {
+
+                NativeAppInstallAdView adView = (NativeAppInstallAdView) getLayoutInflater()
+                        .inflate(R.layout.ad_app_install, null);
+                populateAppInstallAdView(ad, adView);
+                mAdView2.removeAllViews();
+                mAdView2.addView(adView);
+            }
+        });
+
+
+        AdLoader.Builder builder1 = new AdLoader.Builder(this, ADMOB_AD_UNIT_ID);
+
+        builder1.forContentAd(new NativeContentAd.OnContentAdLoadedListener() {
+            @Override
+            public void onContentAdLoaded(NativeContentAd ad) {
+                NativeContentAdView adView = (NativeContentAdView) getLayoutInflater()
+                        .inflate(R.layout.ad_content, null);
+                populateContentAdView(ad, adView);
+                mAdView1.removeAllViews();
+                mAdView1.addView(adView);
+            }
+        });
+
+
+        VideoOptions videoOptions = new VideoOptions.Builder()
+                .setStartMuted(true)
+                .build();
+
+        NativeAdOptions adOptions = new NativeAdOptions.Builder()
+                .setVideoOptions(videoOptions)
+                .build();
+
+        builder1.withNativeAdOptions(adOptions);
+
+        AdLoader adLoader = builder1.withAdListener(new AdListener() {
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+
+            }
+        }).build();
+        adLoader.loadAd(new AdRequest.Builder().build());
+
+        builder2.withNativeAdOptions(adOptions);
+
+        AdLoader adLoader2 = builder2.withAdListener(new AdListener() {
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+
+            }
+        }).build();
+
+        adLoader2.loadAd(new AdRequest.Builder().build());
+    }
+
+    /**
+     * Populates a {@link NativeAppInstallAdView} object with data from a given
+     * {@link NativeAppInstallAd}.
+     *
+     * @param nativeAppInstallAd the object containing the ad's assets
+     * @param adView             the view to be populated
+     */
+    private void populateAppInstallAdView(NativeAppInstallAd nativeAppInstallAd,
+                                          NativeAppInstallAdView adView) {
+        // Get the video controller for the ad. One will always be provided, even if the ad doesn't
+        // have a video asset.
+        VideoController vc = nativeAppInstallAd.getVideoController();
+
+        // Create a new VideoLifecycleCallbacks object and pass it to the VideoController. The
+        // VideoController will call methods on this object when events occur in the video
+        // lifecycle.
+        vc.setVideoLifecycleCallbacks(new VideoController.VideoLifecycleCallbacks() {
+            public void onVideoEnd() {
+                // Publishers should allow native ads to complete video playback before refreshing
+                // or replacing them with another ad in the same UI location.
+                super.onVideoEnd();
+            }
+        });
+
+        adView.setHeadlineView(adView.findViewById(R.id.appinstall_headline));
+        adView.setBodyView(adView.findViewById(R.id.appinstall_body));
+        adView.setCallToActionView(adView.findViewById(R.id.appinstall_call_to_action));
+        adView.setIconView(adView.findViewById(R.id.appinstall_app_icon));
+        adView.setPriceView(adView.findViewById(R.id.appinstall_price));
+        adView.setStarRatingView(adView.findViewById(R.id.appinstall_stars));
+        adView.setStoreView(adView.findViewById(R.id.appinstall_store));
+
+        // Some assets are guaranteed to be in every NativeAppInstallAd.
+        ((TextView) adView.getHeadlineView()).setText(nativeAppInstallAd.getHeadline());
+        ((TextView) adView.getBodyView()).setText(nativeAppInstallAd.getBody());
+        ((Button) adView.getCallToActionView()).setText(nativeAppInstallAd.getCallToAction());
+        ((ImageView) adView.getIconView()).setImageDrawable(
+                nativeAppInstallAd.getIcon().getDrawable());
+
+        MediaView mediaView = adView.findViewById(R.id.appinstall_media);
+        ImageView mainImageView = adView.findViewById(R.id.appinstall_image);
+
+        // Apps can check the VideoController's hasVideoContent property to determine if the
+        // NativeAppInstallAd has a video asset.
+        if (vc.hasVideoContent()) {
+            adView.setMediaView(mediaView);
+            mainImageView.setVisibility(View.GONE);
+
+        } else {
+            adView.setImageView(mainImageView);
+            mediaView.setVisibility(View.GONE);
+
+            // At least one image is guaranteed.
+            List<NativeAd.Image> images = nativeAppInstallAd.getImages();
+            mainImageView.setImageDrawable(images.get(0).getDrawable());
+
+        }
+
+        // These assets aren't guaranteed to be in every NativeAppInstallAd, so it's important to
+        // check before trying to display them.
+        if (nativeAppInstallAd.getPrice() == null) {
+            adView.getPriceView().setVisibility(View.INVISIBLE);
+        } else {
+            adView.getPriceView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getPriceView()).setText(nativeAppInstallAd.getPrice());
+        }
+
+        if (nativeAppInstallAd.getStore() == null) {
+            adView.getStoreView().setVisibility(View.INVISIBLE);
+        } else {
+            adView.getStoreView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getStoreView()).setText(nativeAppInstallAd.getStore());
+        }
+
+        if (nativeAppInstallAd.getStarRating() == null) {
+            adView.getStarRatingView().setVisibility(View.INVISIBLE);
+        } else {
+            ((RatingBar) adView.getStarRatingView())
+                    .setRating(nativeAppInstallAd.getStarRating().floatValue());
+            adView.getStarRatingView().setVisibility(View.VISIBLE);
+        }
+
+        // Assign native ad object to the native view.
+        adView.setNativeAd(nativeAppInstallAd);
+    }
+
+    /**
+     * Populates a {@link NativeContentAdView} object with data from a given
+     * {@link NativeContentAd}.
+     *
+     * @param nativeContentAd the object containing the ad's assets
+     * @param adView          the view to be populated
+     */
+    private void populateContentAdView(NativeContentAd nativeContentAd,
+                                       NativeContentAdView adView) {
+
+        adView.setHeadlineView(adView.findViewById(R.id.contentad_headline));
+        adView.setImageView(adView.findViewById(R.id.contentad_image));
+        adView.setBodyView(adView.findViewById(R.id.contentad_body));
+        adView.setCallToActionView(adView.findViewById(R.id.contentad_call_to_action));
+        adView.setLogoView(adView.findViewById(R.id.contentad_logo));
+        adView.setAdvertiserView(adView.findViewById(R.id.contentad_advertiser));
+
+        // Some assets are guaranteed to be in every NativeContentAd.
+        ((TextView) adView.getHeadlineView()).setText(nativeContentAd.getHeadline());
+        ((TextView) adView.getBodyView()).setText(nativeContentAd.getBody());
+        ((TextView) adView.getCallToActionView()).setText(nativeContentAd.getCallToAction());
+        ((TextView) adView.getAdvertiserView()).setText(nativeContentAd.getAdvertiser());
+
+        List<NativeAd.Image> images = nativeContentAd.getImages();
+
+        if (images.size() > 0) {
+            ((ImageView) adView.getImageView()).setImageDrawable(images.get(0).getDrawable());
+        }
+
+        // Some aren't guaranteed, however, and should be checked.
+        NativeAd.Image logoImage = nativeContentAd.getLogo();
+
+        if (logoImage == null) {
+            adView.getLogoView().setVisibility(View.INVISIBLE);
+        } else {
+            ((ImageView) adView.getLogoView()).setImageDrawable(logoImage.getDrawable());
+            adView.getLogoView().setVisibility(View.VISIBLE);
+        }
+
+        // Assign native ad object to the native view.
+        adView.setNativeAd(nativeContentAd);
     }
 
     private void setArticleText() {
